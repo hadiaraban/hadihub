c633d9a7e29e90d09b61c937e8c1fbfb264a4a33
Hadi Araban <hadi.araban@gmail.com>
2025-03-09 17:22:31 +0330

    create template
    create template for about , contact and home page

--- website/views.py
+++ website/views.py
@@ -1,8 +1,9 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def index_view(request):
-    return HttpResponse('Home page')
+    return render(request,'index.html')
 def about_view(request):
-    return HttpResponse('About page')
+    return render(request,'about.html')
 def contact_view(request):
-    return HttpResponse('Contact page')
+    return render(request,'contact.html')
